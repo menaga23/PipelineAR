4cd3d07d126e884dfc97a0f013f9844582d77a2e
R.K.Menaga <menagark2005@gmail.com>
2026-03-13 12:30:35 +0530

    Fixed pipeline spawning on tap

--- Assets/Scripts/ARTouchPlacer.cs
+++ Assets/Scripts/ARTouchPlacer.cs
@@ -7,32 +7,26 @@ public class ARTouchPlacer : MonoBehaviour
 {
     public ARRaycastManager raycastManager;
     public PipelineSpawner pipelineSpawner;
-
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    private bool pipelinesPlaced = false;
 
     void Update()
     {
-        if (pipelinesPlaced) return;
-
-        if (Input.touchCount > 0 && 
+        if (Input.touchCount > 0 &&
             Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Touch touch = Input.GetTouch(0);
-            if (raycastManager.Raycast(touch.position, hits, 
-                TrackableType.PlaneWithinPolygon))
+            if (raycastManager.Raycast(touch.position, hits,
+                TrackableType.AllTypes))
             {
                 Pose hitPose = hits[0].pose;
                 pipelineSpawner.transform.position = hitPose.position;
                 pipelineSpawner.SpawnDemoPipelines();
-                pipelinesPlaced = true;
             }
         }
     }
 
     public void ResetPlacement()
     {
-        pipelinesPlaced = false;
         pipelineSpawner.ClearAllPipelines();
     }
 }
